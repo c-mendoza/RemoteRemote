81ce973c9efe09922434b086fe090fee253146ac
c-mendoza <c@matadata.com>
2021-03-15 17:34:56 -0400

    Detect parameters prior to showing OFParameterGroupView

--- lib/pages/parameter_controller_page.dart
+++ lib/pages/parameter_controller_page.dart
@@ -4,22 +4,37 @@ import 'package:provider/provider.dart';
 import 'package:remote_remote/constants.dart';
 import 'package:remote_remote/of_parameter_controller/widgets/of_group_view.dart';
 
-class ParameterControllerPage extends StatelessWidget {
+import '../app_model.dart';
 
+class ParameterControllerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Navigator(
-      key: kParamNavigatorKey,
-      initialRoute: '/',
-      onGenerateRoute: (RouteSettings settings) {
-        return MaterialPageRoute(
-            builder: (context) => OFParameterGroupView(settings.name));
-      },
-    );
+    final appModel = Provider.of<AppModel>(context, listen: false);
+
+    if (appModel.parametersReady) {
+      return Navigator(
+        key: kParamNavigatorKey,
+        initialRoute: '/',
+        onGenerateRoute: (RouteSettings settings) {
+          return MaterialPageRoute(
+              builder: (context) => OFParameterGroupView(settings.name));
+        },
+      );
+    } else {
+      return SizedBox.expand(
+        child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Text(
+          'No parameters available.\n\nPlease go to the Status page and press Connect',
+          style: Theme.of(context).textTheme.headline5, textAlign: TextAlign.center,
+        ),
+            )),
+      );
+    }
   }
 }
 
 Widget bla;
-
 
 //class _
